4537dfa7988ccff27a919b82353c3917ca5a3dbc
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-18 16:33:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
-  
+  count = 0 
+  while count < array.length do
+  if  value_to_find == array[count]
+    puts value_to_find
+  else nil 
+    count += 1
+  end
 end
